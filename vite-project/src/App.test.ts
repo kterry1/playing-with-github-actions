43c81eda52fae174c553706e42e1b8c3f6047b7a
Kevin Terry <kevin.terry@commercetools.com>
2024-12-17 16:41:14 -0500

    added vite-project and vitest

---
+++ vite-project/src/App.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+describe("Testing", () => {
+  it.todo("Eventually I'll write this");
+  it("checks that 1 equals 1", () => {
+    expect(1).toBe(1);
+  });
+});
